d60e271d84bb739523e15621704a4c8e7f231e60
nazlitirek <tireknazlihilal@gmail.com>
2024-12-24 14:01:17 +0300

    first

---
+++ Data/Enums/MovieCategory.cs
@@ -0,0 +1,10 @@
+﻿namespace eCommerce.Data.Enums
+{
+    public enum MovieCategory
+    {
+        Action = 0,
+        Comedy,
+        Drama,
+        Documentary
+    }
+}
---
+++ Models/Producer.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace eCommerce.Models
+{
+	public class Producer
+	{
+		[Key]
+		public int Id { get; set; }
+		public string ProfilePictureUrl { get; set; }
+		public string FullName { get; set; }
+		public string Bio { get; set; }
+
+		//Relationships
+		public List<Movie> Movies { get; set; }
+
+}
