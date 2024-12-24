10dfbdd8a2c0af8c88fa5e0fd926d6ea2b97d35b
nazlitirek <tireknazlihilal@gmail.com>
2024-12-24 14:21:22 +0300

    db done

--- obj/Debug/net8.0/eCommerce.AssemblyInfo.cs
+++ obj/Debug/net8.0/eCommerce.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("eCommerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d60e271d84bb739523e15621704a4c8e7f231e60")]
 [assembly: System.Reflection.AssemblyProductAttribute("eCommerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("eCommerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
